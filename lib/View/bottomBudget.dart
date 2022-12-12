94446dd2f17cfe2bcff91fded45e49839466422a
unknown <vi12340>
2022-12-12 10:38:23 +0700

    update

--- lib/View/bottomBudget.dart
+++ lib/View/bottomBudget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class bottomBudget extends StatefulWidget {
   const bottomBudget({super.key});
@@ -12,14 +13,14 @@ class _bottomBudgetState extends State<bottomBudget> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          actions: [Icon(Icons.exit_to_app)],
+          leading: IconButton(
+              onPressed: (() {
+                Navigator.pop(context);
+              }),
+              icon: Icon(Icons.close)),
         ),
         body: Row(
-          children: [
-            TextField(
-                decoration: InputDecoration(border: OutlineInputBorder())),
-            ElevatedButton(onPressed: (() {}), child: Text('Save')),
-          ],
+          children: [],
         ));
   }
 }
